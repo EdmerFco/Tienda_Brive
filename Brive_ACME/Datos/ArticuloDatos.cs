6db1dc2181a4a6e21069fdf6febbad67c6d81586
EdmerFco <83522812+EdmerFco@users.noreply.github.com>
2022-07-02 23:42:12 -0500

    Squashed commit of the following:
    commit 4dbb022ef4b87be8f94879ecc3bde190df984497
    Author: EdmerFco <83522812+EdmerFco@users.noreply.github.com>
    Date:   Sat Jul 2 23:41:07 2022 -0500
    
        Conexion.ADO

---
+++ Brive_ACME/Datos/ArticuloDatos.cs
@@ -0,0 +1,42 @@
+﻿using Brive_ACME.Models;
+using System.Data.SqlClient;
+
+namespace Brive_ACME.Datos
+{
+    public class ArticuloDatos : Conexion
+    {
+        public IEnumerable<Articulo> Consultar()
+        {
+            List<Articulo> articulos = new List<Articulo>();
+            Conectar();
+            try
+            {
+                SqlCommand comando = new SqlCommand("Consultar_Articulo", cnn);
+                comando.CommandType = System.Data.CommandType.StoredProcedure;
+                SqlDataReader lector = comando.ExecuteReader();
+                while (lector.Read())
+                {
+                    Articulo modelo = new Articulo()
+                    {
+                        idArticulo = int.Parse(lector[0].ToString()),
+                        codigo = int.Parse(lector[1].ToString()),
+                        nombre = lector[2].ToString(),
+                        Precio_Venta = int.Parse(lector[3].ToString()),
+                        Estado = lector[4].ToString(),
+                        idCategoria = int.Parse(lector[5].ToString())
+                    };
+                    articulos.Add(modelo);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+            finally
+            {
+                Desconexion();
+            }
+            return articulos;
+        }
+    }
+}
---
+++ Brive_ACME/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Brive_ACME.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
